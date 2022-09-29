bde2baded9fe2fe9bc0b44fab351a63748c6da7a
Gregory Weaver <gregory.weaver@gmail.com>
2022-09-29 13:44:06 -0700

    docs inline.

--- CodingTest.java
+++ CodingTest.java
@@ -3,6 +3,20 @@ import java.util.HashMap;
 
 public class CodingTest {
 
+    /** 
+        Given two strings of arbitrary length as input, S1 is capital letters. S2 is list of animals separated by spaces.
+        Describe an algorithm that will check for a pattern equivilancy between the strings. 
+                Ex. S1="ABBA" S2="CAT DOG DOG CAT". Show that the pattern is equivilant. 
+                + "ABBA" = "CAT DOG DOG CAT" > true 
+                + "ABBA" = "CAT CAT DOG CAT" > false
+                + "ABBBAA" = "CAT DOG DOG DOG CAT CAT" > true
+                + "ABBZ" = "CAT DOG DOG ELEPHANT" > true
+
+        @param keys key string S1
+        @param values value string S2
+ 
+        @return true if key string and data string match
+     */
     public static boolean isMatch(String keys, String values) {
         HashMap matchMap = new HashMap<>();
         String[] explodedValues = values.split(" "); 
@@ -31,7 +45,7 @@ public class CodingTest {
     }
 
     /** 
-        Example usage: `java CodingTest ABBA "CAT DOG DOG CAT"
+        Example usage: `java CodingTest ABBA "CAT DOG DOG CAT"`
      */
     public static void main(String[] args) {
         String keys = args[0];
